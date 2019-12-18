ce8fc92b9753d42804705386e8833a3e8dc8978f
porapio <porada8@wp.pl>
2019-12-18 10:35:18 +0100

    Update bank_account.py

--- bank_account.py
+++ bank_account.py
@@ -2,7 +2,6 @@ import threading
 
 class BankAccount(object):
     lock = threading.Lock()
-    x = 0
 
     def __init__(self):
         self.is_open = False
@@ -12,16 +11,10 @@ class BankAccount(object):
         if self.is_open:
             return self.balance
         else:
-            # I like this exception
             raise ExceptionError("closed")
-        
 
     def open(self):
-<<<<<<< HEAD
         if self.is_open == False
-=======
-        if self.is_open == False:
->>>>>>> 92e89b2db2b85187ea181e497bdfb1e3de374582
             raise ValueError("Already opened")
         self.balance = 0
         self.is_open = True
@@ -47,8 +40,4 @@ class BankAccount(object):
     def close(self):
         if self.is_open == False:
             raise ValueError("Already closed")
-<<<<<<< HEAD
         self.is_open = True
-=======
-        self.is_open = False
->>>>>>> 92e89b2db2b85187ea181e497bdfb1e3de374582
